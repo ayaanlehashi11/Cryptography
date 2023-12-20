73fd8ea4d48a89f36c627006831ca4bc5166969d
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-20 06:20:43 -0800

    Update ascii.c

--- encoding/ascii.c
+++ encoding/ascii.c
@@ -1,1 +1,12 @@
 
+#include <stdio.h>
+
+int main(int argc , char ** argv)
+{
+   char ch;
+   printf("Enter the string to be converted to it's corresponding int value: ");
+   scanf("%c" , &ch);
+   // %d displays the integer value of a character
+   // %c the character
+   printf("the ascii value of the c variable is " , ch);
+}
