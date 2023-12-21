508a1c007476d4461580aca05c4e747420f4dbb9
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2023-12-21 21:30:42 +0300

    Update common.hpp

--- symmetric/common.hpp
+++ symmetric/common.hpp
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 #include <openssl/evp.h>
 
-int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, EVP_CIPHER_CTX *e_ctx, 
+int aes_init(unsigned char *key_data, int key_data_len, 
+             unsigned char *salt, 
+             EVP_CIPHER_CTX *e_ctx, 
              EVP_CIPHER_CTX *d_ctx);
 
-unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len);
+std::vector<unsigned char> aes_128_gcm_encrypt(std::string plaintext, std::string key);
 
-unsigned char *aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len);
+std::string aes_128_gcm_decrypt(std::vector<unsigned char> ciphertext, std::string key);
+
+
+
 
 
 
